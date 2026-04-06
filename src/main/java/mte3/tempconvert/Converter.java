a09bb3a8b837b97bbdbbddd27e56c1d0b2743c8b
audra-hossain <ash4484@rit.edu>
2026-04-06 10:35:34 -0400

    q1

--- src/main/java/mte3/tempconvert/Converter.java
+++ src/main/java/mte3/tempconvert/Converter.java
@@ -17,15 +17,18 @@ public class Converter {
         
         try(Scanner scanner = new Scanner(System.in)) {
             System.out.print("Please enter temperature (for conversion): ");
-
+            int currenttemp = scanner.nextInt();
             // (part 1) conversion from C to F
-            
-            
+            CelsiusToFahrenheit cf = new CelsiusToFahrenheit();
+            System.out.println("Celsius to Fahrenheit: "+cf.convert(currenttemp));
             // (part 2) conversion from F to C
-            
-            
+            TempConvert fc = new TempConvert() {
+                @Override
+                public double convert(double temp){return((temp-32)*(5/9));}
+            };
+            System.out.println("Fahrenheit to Celsius: "+fc.convert(currenttemp));
             // (part 3) conversion from F to K
-            
+            // TempConvert fk = new TempConvert(currenttemp) -> return((currenttemp-32)*(5/9)+273.15);
 
 
         }  // try { } block closed
