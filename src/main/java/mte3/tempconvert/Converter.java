2c725c59903c8c531aec40cee8faeb8452a9e806
audra-hossain <ash4484@rit.edu>
2026-04-06 11:24:43 -0400

    q1

--- src/main/java/mte3/tempconvert/Converter.java
+++ src/main/java/mte3/tempconvert/Converter.java
@@ -17,18 +17,26 @@ public class Converter {
         
         try(Scanner scanner = new Scanner(System.in)) {
             System.out.print("Please enter temperature (for conversion): ");
-            int currenttemp = scanner.nextInt();
+            // int currenttemp = scanner.nextInt();
+            double original = scanner.nextDouble();
+
             // (part 1) conversion from C to F
             CelsiusToFahrenheit cf = new CelsiusToFahrenheit();
-            System.out.println("Celsius to Fahrenheit: "+cf.convert(currenttemp));
+            // System.out.println("Celsius to Fahrenheit: " + cf.convert(currenttemp));
+            System.out.println("Celsius to Fahrenheit: " + cf.convert(original));
+            
             // (part 2) conversion from F to C
             TempConvert fc = new TempConvert() {
                 @Override
-                public double convert(double temp){return((temp-32)*(5/9));}
+                public double convert(double temp) { return ((temp - 32) * (5.0 / 9.0)); }
             };
-            System.out.println("Fahrenheit to Celsius: "+fc.convert(currenttemp));
+            // System.out.println("Fahrenheit to Celsius: "+fc.convert(currenttemp));
+            System.out.println("Fahrenheit to Celsius: "+ fc.convert(original));
+
             // (part 3) conversion from F to K
-            // TempConvert fk = new TempConvert(currenttemp) -> return((currenttemp-32)*(5/9)+273.15);
+            TempConvert fk = (temp) -> ((temp - 32)*(5.0 / 9.0) + 273.15);
+            // System.out.println("Fahrenheit to Kelvin: "+ fk.convert(currenttemp));
+            System.out.println("Fahrenheit to Kelvin: "+ fk.convert(original));
 
 
         }  // try { } block closed
